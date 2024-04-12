20fce044d99fafbae3a9dade4884861fcff58cc8
Altus <altusrossouw2@gmail.com>
2024-04-12 21:33:40 +0200

    Initial Commit

---
+++ MyselfInSixWords/MyselfInSixWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyselfInSixWordsApp.swift
+//  MyselfInSixWords
+//
+//  Created by Altus Rossouw on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyselfInSixWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
